cac52ae4736f15f794e788d5fccd7e615419acf4
Melissa <Melissa@DESKTOP-79FQADB>
2023-11-06 21:57:33 -0300

    adicionando a classe de livro e criando um array list para cadastro

---
+++ Sistema_CompartiLivros/src/CadastroLivros.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Melissa
+ */
+public class CadastroLivros {
+    public static void main(String[] args) {
+        List<Livro> livros = new ArrayList<>();
+        livros.add(new Livro (1, "Os contos dos pássaros","Maria", "Reccoo"));
+    }
+}
