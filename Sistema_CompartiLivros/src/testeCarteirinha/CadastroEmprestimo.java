0a682ebd4ab9597effbd89bd19ac6b02e2b1df49
mwlissabr <mwlissabr@gmail.com>
2023-11-09 19:27:01 -0300

    instanciando o array de forma correta

--- Sistema_CompartiLivros/src/testeCarteirinha/CadastroEmprestimo.java
+++ Sistema_CompartiLivros/src/testeCarteirinha/CadastroEmprestimo.java
@@ -11,6 +11,7 @@ import java.util.List;
  * @author melis
  */
 public class CadastroEmprestimo {
+
     public static void main(String[] args) {
         Carteirinha c1 = new Carteirinha(1, "Lucas", "4499999999", "Rua das Flores", "021213325");
         Carteirinha c2 = new Carteirinha(2, "Melissa", "4499888888", "Rua das Flores 51", "31353625");
@@ -20,7 +21,7 @@ public class CadastroEmprestimo {
         livros.add(new Livro (2, "Os contos dos golfinhos","João", "Reccoo"));
         
         List<Emprestimo> novoEmprestimo = new ArrayList<>();
-        novoEmprestimo.add(new Emprestimo(1, "07/11/2023", "20/11/2023", c2, emprestados))
+        novoEmprestimo.add(new Emprestimo(1, "07/11/2023", "20/11/2023", c2, (ArrayList<Livro>) livros));
         
         
         System.out.println(novoEmprestimo);
